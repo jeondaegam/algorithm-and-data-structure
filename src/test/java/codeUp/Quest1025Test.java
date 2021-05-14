fd17ada64bf98bc2b8e6fa254ba271202a248223
jeondaegam <jeondaegam@naver.com>
2021-05-14 21:55:39 +0900

    [1025] 다섯 자리의 정수 1개를 입력받아 각 자리별로 나누어 출력하기

---
+++ src/test/java/codeUp/Quest1025Test.java
@@ -0,0 +1,15 @@
+package codeUp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Quest1025Test {
+
+    @Test
+    void testRun(){
+        Quest1025 quest = new Quest1025();
+        quest.divideNumber(54321);
+    }
+
+}
