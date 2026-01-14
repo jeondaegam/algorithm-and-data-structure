004df242a089a822c805f2fb543258a59ed498e9
yeoreum <jeondaegam@naver.com>
2026-01-14 09:38:37 +0900

    스터디: 단어 뒤집기, 대소문자 변환

--- src/main/java/studyWithSh/Q_2.java
+++ src/main/java/studyWithSh/Q_2.java
@@ -6,30 +6,31 @@ import java.io.InputStreamReader;
 
 public class Q_2 {
     /**
-     * 문장 속 단어
+     * 대소문자 변환
      */
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] arr = br.readLine().split(" ");
-        int answer = 0;
+        String input = br.readLine();
 
-        for (int i = 0; i < arr.length; i++) {
-            String a = arr[answer];
-            String b = arr[i];
+        if (input.length() > 100) {
+            return;
+        }
 
-            if (b.length() > a.length()) {
-                answer = i;
+        String answer = solution(input);
+        System.out.println(answer);
+    }
+
+    public static String solution(String input) {
+        StringBuilder answer = new StringBuilder();
+
+        for (char c : input.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                answer.append(Character.toLowerCase(c));
+            } else {
+                answer.append(Character.toUpperCase(c));
             }
         }
-        System.out.println(arr[answer]);
+
+        return answer.toString();
     }
 }
-
-// 1. 문자열을 입력받는다.
-// 2. 공백을 기준으로 split 하여 배열에 저장한다.
-// 3. 차례대로 length를 비교한다.
-// 4. 길이가 더 긴 단어를 만나면 배열의 인덱스를 저장한다.
-// 5. 출력한다.
-// 시간복잡도 O(N)
-
---
+++ src/main/java/studyWithSh/Q_4.java
@@ -0,0 +1,27 @@
+package studyWithSh;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q_4 {
+    /**
+     * 단어 뒤집기
+     */
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if (n < 3 || n > 20) return;
+
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+            String reserved = solution(line);
+            System.out.println(reserved);
+        }
+    }
+
+    public static String solution(String line) {
+        return new StringBuilder(line).reverse().toString();
+    }
+}
